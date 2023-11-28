802f41a360040d881c0bc8d10b338a1d17df73f9
ernesto4664 <ernesto4664@gmail.com>
2023-11-28 18:04:47 -0300

    mejorando

--- app/Http/Controllers/TramitesDigitalesController.php
+++ app/Http/Controllers/TramitesDigitalesController.php
@@ -1,10 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 use App\Models\TramitesDigitales;
 use App\Models\TramitesDigitalesDocs;
-use Illuminate\Http\Request;
+
 
 class TramitesDigitalesController extends Controller{
 
