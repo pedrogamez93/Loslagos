36491108d35eee71a3ac95b53b147f822464aae4
ernesto4664 <ernesto4664@gmail.com>
2023-11-29 22:24:51 -0300

    mejorando la insercion de fecha

--- app/Http/Controllers/TramitesDigitalesController.php
+++ app/Http/Controllers/TramitesDigitalesController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\TramitesDigitales;
 use App\Models\TramitesDigitalesDocs;
+use Carbon\Carbon;
 
 
 class TramitesDigitalesController extends Controller{
