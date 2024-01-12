2022f08ae884807301fbf06e7a8bafa688d40c60
fguedez12 <148079749+fguedez12@users.noreply.github.com>
2024-01-12 01:21:47 -0300

    modificacion del login

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -210,7 +210,7 @@ button.btn.btn-primary.Accede {
 }
 input#email,input#password {
     border-radius: 100px;
-    border: 1px;
+    border: 1px solid #565656;
     padding-left: 25px;
 }
 .colorB {
