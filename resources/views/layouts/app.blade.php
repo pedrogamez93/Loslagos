0885698d43730a437891de3ea9b60f2e8da3e649
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-01-15 08:52:02 -0400

    atualizacion home

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -211,64 +211,64 @@
     </div>
   </div>
 </div>
-<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-4 border-top " style="">
-    
+<footer class="row row-cols-1 row-cols-sm-2 row-cols-md-4 border-top" style="">
     <div class="col mb-3">
-    <a href="#" class="" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
-Accesibilidad
-      </a>
-      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-        <img src="{{ asset('storage/img/logo_gore_vertical-blanco.png') }}" >
-      </a>
-      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
-        <img src="{{ asset('storage/img/Logo-Core-blanco.png') }}" >
-      </a>
-    </div>
-
-    
-
-    <div class="col mb-3 ">
-      <h5>Menu</h5>
-      <ul class="nav flex-column ">
-        <li class="nav-item mb-2 text-light">
-            <a href="#" class="nav-link p-0 text-muted">Gobierno Regional</a>
-         
-        </li>
-        <li class="nav-item mb-2 "><a href="#" class="nav-link p-0 text-muted">Consejo Regional</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Región de los Lagos</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Directorio de Funciones</a></li>
-      
-      </ul>
+        <a href="#" class="" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Accesibilidad
+        </a>
+        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+            <img src="{{ asset('storage/img/logo_gore_vertical-blanco.png') }}" alt="Logo GORE Vertical Blanco">
+        </a>
+        <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+            <img src="{{ asset('storage/img/Logo-Core-blanco.png') }}" alt="Logo Core Blanco">
+        </a>
     </div>
 
     <div class="col mb-3">
-      <h5>Enlaces directos</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Fondo comunidad (Evaluación)</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Creando y Cuidando la Región</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Información de la región</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Directorio de funcionarios</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Qué es el Gobierno Regional</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Organigrama</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Trámites</a></li>
-      </ul>
+        <h5 class="footer-col-title">Menú</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2 text-light">
+                <a href="#" class="nav-link p-0 text-muted">Gobierno Regional</a>
+            </li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Consejo Regional</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Región de los Lagos</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Directorio de Funciones</a></li>
+        </ul>
     </div>
 
     <div class="col mb-3">
-      <h5>Contacta con nosotras aquí</h5>
-      <ul class="nav flex-column">
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Contacta con nosotros aquí</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Comunícates con nosotros a través nuestro correo electrónico y nos pondremos en contacto de inmediato</a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted"><div class="d-flex flex-column flex-sm-row w-100 gap-2">
-            <label for="newsletter1" class="visually-hidden">Escribenos</label>
-            <input id="newsletter1" type="text" class="form-control" placeholder="Escribenos">
-            <button class="btn btn-primary" type="button">Subscribe</button>
-          </div></a></li>
-        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Copyright © GORE Los Lagos 2023</a></li>
-      
-      </ul>
+        <h5 class="footer-col-title" >Enlaces directos</h5>
+        <ul class="nav flex-column">
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Fondo comunidad (Evaluación)</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Creando y Cuidando la Región</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Información de la región</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Directorio de funcionarios</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Qué es el Gobierno Regional</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Organigrama</a></li>
+            <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Trámites</a></li>
+        </ul>
     </div>
-  </footer>  
+
+    <div class="col mb-3">
+        <h5 class="footer-col-title" >Contacta con nosotros aquí</h5>
+        <ul class="nav flex-column">
+            
+            <li class="nav-item mb-4"><a href="#" class="nav-link p-0 text-muted">Comunícate con nosotros a través de nuestro correo electrónico y nos pondremos en contacto de inmediato</a></li>
+            <li class="nav-item mb-2">
+            <a href="#" class="nav-link p-0 text-muted">
+                <div class="d-flex flex-column flex-sm-row w-100 gap-2" style="border-radius: 100px; background: rgba(255, 255, 255, 0.20);">
+                    <label for="newsletter1" class="visually-hidden" style="color:white">Escríbenos</label>
+                    <input id="newsletter12" type="text" class="form-control" placeholder="Escríbenos" style="border-radius: 100px; background: rgba(255, 255, 255, 0.20); color: white;">
+
+                    <button class="btn " type="button" style="color: white;">Suscribirse</button>
+                </div>
+            </a>
+            </li>
+            <li class="nav-item mt-3"><a href="#" class="nav-link p-0 text-muted">Copyright © GORE Los Lagos 2023</a></li>
+        </ul>
+    </div>
+</footer>
+  
     <!-- JavaScripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
