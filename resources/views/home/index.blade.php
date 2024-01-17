dc46c79b64d1ddcd0233e4f927e481dd935bd8a3
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-01-17 17:01:49 -0400

    Update index.blade.php

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -98,7 +98,9 @@
             <img src="{{ route('mostrar.imagen', ['carpeta' => 'salaprensa', 'imagen' => basename($registro->archivo_path)]) }}" class="img-fluid" alt="{{ $registro->titulo }}" style="width: 300px; height: 292px; border-radius: 18px;">
             <div class="carousel-caption">
                 <h5 style="" class="tituloprensa">{{ Str::limit($registro->titulo, 20) }}</h5>
-               
+                @if($registro->descripcion)
+                    <p style="" class="descripcionprensa">{{ Str::limit(strip_tags($registro->descripcion), 20) }}</p>
+                @endif
                 <a style="right: 1px; bottom: 0px; position: absolute;" class="irnoticia">ir ahora <i class="bi bi-arrow-right"></i></a>
             </div>
         </div>
@@ -150,7 +152,7 @@ Actividades planeadas que involucran un accionar más amplio que un proyecto...
                     
                 </div>
                 <h5 class="titulotramites">{{ $tramite->titulo }}</h5>
-                    <p class="descripciontramites">{{ $tramite->descripcion }}</p>
+                    <p class="descripciontramites">{{ Str::limit(strip_tags($registro->descripcion), 20) }}</p>
             </div>
         @endforeach
     </div>
