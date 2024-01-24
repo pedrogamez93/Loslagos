8f545c78c45f08783197a7812fad14111229fe80
fguedez12 <148079749+fguedez12@users.noreply.github.com>
2024-01-24 19:52:17 -0300

    modificaciones de margenes

--- resources/views/acerca/estrategiaregional.blade.php
+++ resources/views/acerca/estrategiaregional.blade.php
@@ -142,18 +142,18 @@
     <div class="row">
         <div class="col-md-12">
             <div class="container second content-breadc pt-5 pb-5">
-                <div class="row" style="padding: 10px 0px 20px 55px;">
-                    <div class="col-md-12" style="padding: 0;">
+                <div class="row">
+                    <div class="col-md-12 p-0">
                         <p class="style-bread"><a href="http://127.0.0.1:8000/">Home </a>/<a href="/gobiernoregional/acerca"> Gobierno Regional </a>/ <a href="/gobiernoregional/acerca"> Acerca</a> / <span style="font-Weight: 700;"><a href="/gobiernoregional/acerca/estrategiaregional">Estrategia Regional</a></span></p>
                     </div>
                 </div>
                     
-                <div class="container content-prin pt-4">
-                    <div class="row" style="padding: 10px 0px 0px 25px;">
-                        <div class="col-md-12">
+                <div class="container content-prin pt-4  p-0">
+                    <div class="row">
+                        <div class="col-md-12 p-0">
                             <p class="one-title pb-5">Gobierno Regional</p>
                         </div>
-                        <div class="col-md-6">
+                        <div class="col-md-6 p-0">
                             <p>El Gobierno Regional (GORE) es un organismo autónomo, que tiene por objetivo la administración de la región, impulsando su desarrollo económico, cultural y social</p>
                         </div>
                     </div>
@@ -168,7 +168,7 @@
             <div class="col-md-12 pt-4 pb-4">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-12" style="padding: 0 0 0 2.9rem;">
+                        <div class="col-md-12 p-0">
                             <p class="title-cat">Selecciona una Categoría</p>
                         </div>
                     </div>
@@ -178,8 +178,8 @@
     </div>
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="container set">
+            <div class="col-md-12 p-0">
+                <div class="container set p-0">
                 @include('layouts.listacategorias')
                 </div>
                 <div class="container mt-5 mb-5">
@@ -187,7 +187,7 @@
                         <div class="col-md-6">
                             <div class="container int">
                                 <div class="row">
-                                    <div class="col-md-12">
+                                    <div class="col-md-12 p-0 pr-2">
                                         <p class="style-tag pt-4 pb-4">{{ $estrategia->tag_comentario }}</p>
                                         <p class="title-cat pt-2 pb-2">{{ $estrategia->titulo }}</p>
                                         <p class="style-down pt-2 pb-5">{{ $estrategia->bajada }}</p>
