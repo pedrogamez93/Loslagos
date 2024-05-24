d853b21c9ac1bdd6a7d0bf86ad4c60ad6ba372e6
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-05-24 07:08:52 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -492,10 +492,10 @@
 </script>
 
 <script>
-      $(document).ready(function () {
+       $(document).ready(function () {
             // Función para limpiar el texto
             function limpiarTexto(texto) {
-                return texto.replace(/\\r\\n/g, "<br>").replace(/\\n/g, "<br>").replace(/\\r/g, "<br>");
+                return texto.replace(/\\\\r\\\\n\\\\r\\\\n/g, "<br>").replace(/\\\\r\\\\n/g, "<br>").replace(/\\\\n/g, "<br>").replace(/\\\\r/g, "<br>");
             }
 
             // Función para recorrer todos los nodos de texto y limpiarlos
@@ -507,7 +507,7 @@
                         if (index > 0) {
                             fragmento.appendChild(document.createElement('br'));
                         }
-                        fragmento.appendChild(document.createTextNode(parte));
+                        fragmento.appendChild(document.createTextNode(parte.replace(/\\/g, '')));
                     });
                     node.parentNode.replaceChild(fragmento, node);
                 } else {
