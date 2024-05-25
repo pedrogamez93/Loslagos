06e20f1f81451c8c68a790bb73dce445ae1acb41
ernesto4664 <ernesto4664@gmail.com>
2024-05-25 14:41:50 -0400

    mejorando descargas en concurso publico

--- resources/views/concursopublico.blade.php
+++ resources/views/concursopublico.blade.php
@@ -252,7 +252,7 @@
                                     <div class="accordion-body documentos-container">
                                         @foreach ($concurso->documentos as $documento)
                                         <div class="mi-documento mt-3 mb-3">
-                                            <a href="{{ asset('storage/' . $documento['ruta_documento']) }}" target="_blank">
+                                            <a href="{{ route('downloadconcursopublico', $documento->id) }}" target="_blank">
                                                 <img width="43px" height="44px" src="{{ asset('storage/images/pdf.png') }}" alt="Descripción de la imagen" style="display: inline-block; vertical-align: middle;">
                                                     <p class="p-doc mt-2 mb-2" style="font-family: 'Inter'; font-weight: 500; font-size: 16px; line-height: 19.36px; display: inline-block; vertical-align: middle; color:#565656; width: 950px;">{{ $documento->nombre_documento }}</p>
                                             </a>
