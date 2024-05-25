514e39386dfb6a62fd842a69fc9878f539c1eab5
ernesto4664 <ernesto4664@gmail.com>
2024-05-25 16:58:40 -0400

    img en landings

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -242,19 +242,7 @@ class LandingController extends Controller{
 
     public function showImage($filename)
     {
-        $path = storage_path('app/public/landing_images/' . $filename);
-    
-        if (!File::exists($path)) {
-            abort(404);
-        }
-    
-        $file = File::get($path);
-        $type = File::mimeType($path);
-    
-        $response = Response::make($file, 200);
-        $response->header("Content-Type", $type);
-    
-        return $response;
+        return response()->file(storage_path('app/public/landing_images/' . $filename));
     }
 
     public function downloaddocslanding($id)
