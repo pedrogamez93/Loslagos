2ba49cd0f94863942bf673692a2f6bf238c0e30e
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-05-27 10:09:34 -0400

    Update tabladocumentos.blade.php

--- resources/views/documentos/tabladocumentos.blade.php
+++ resources/views/documentos/tabladocumentos.blade.php
@@ -126,7 +126,7 @@ input:required {
                                 <td>{{ $documento->lugar }}</td>
                                 <td>{{ $documento->created_at }}</td>
                                 <td>
-                                    <a href="{{ route('descargar.archivo', ['archivo' => basename($documento->archivo_path)]) }}" class="btn btn-primary" download>Descargar</a>
+                                    <a href="{{ route('documentos.download', ['id' => $documento->id]) }}" class="btn btn-primary" download>Descargar</a>
                                     <a href="{{ route('documentos.edit', ['id' => $documento->id]) }}" class="btn btn-warning">Editar</a>
                                     <form action="{{ route('documentos.destroy', ['id' => $documento->id]) }}" method="POST" style="display: inline;">
                                         @csrf
