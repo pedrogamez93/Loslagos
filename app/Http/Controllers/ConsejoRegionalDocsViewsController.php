3985f05e31c4f4783f689675551090e8497f02ea
ernesto4664 <ernesto4664@gmail.com>
2024-06-27 17:24:21 -0400

    mejorando

--- app/Http/Controllers/ConsejoRegionalDocsViewsController.php
+++ app/Http/Controllers/ConsejoRegionalDocsViewsController.php
@@ -180,7 +180,6 @@ public function Indexcertificadosdeacuerdos(Request $request)
 
 public function showFiltroAno($anio)
 {
-    dd('hola');
     // Obtener los años únicos de la tabla documentos_sesiones
     $anios = Documento_Sesion::selectRaw('EXTRACT(YEAR FROM fechadoc) AS anio')
                              ->groupBy('anio')
