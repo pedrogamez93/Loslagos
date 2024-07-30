288ee735e43bef6d7f11d285e5cfcd26a0a100e1
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-07-29 23:22:51 -0400

    Update edit.blade.php

--- resources/views/programas/edit.blade.php
+++ resources/views/programas/edit.blade.php
@@ -1,10 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Editar Programa</title>
+<title>Editar Programa</title>
 <!-- Jquery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <!-- Bootstrap CSS y JS -->
@@ -66,7 +61,6 @@
         font-weight: 500;
     }
 </style>
-</head>
 <body>
 <div class="container-fluid body">
     <div class="row">
@@ -330,7 +324,6 @@
     </div>
 </div>
 </body>
-</html>
 
 <script>
       tinymce.init({
