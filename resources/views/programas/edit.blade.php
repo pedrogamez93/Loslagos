8d447704b86688e1d099f5597842295ed2722134
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-07-29 22:55:47 -0400

    Update edit.blade.php

--- resources/views/programas/edit.blade.php
+++ resources/views/programas/edit.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="Content-Type" content="text/html;charset=ISO-8859-1">
 
     <title>Editar Programa</title>
 <!-- Jquery -->
