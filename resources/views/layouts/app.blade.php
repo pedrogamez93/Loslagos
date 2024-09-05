1a2e2ba707fa7ede9817ff19e3868cd0f347d2ae
Pedrogamez93 <Pedrogamez93@hotmail.com>
2024-09-05 11:47:33 -0400

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -279,13 +279,7 @@
 
 </nav>
 
-<div class="botonloginmovil">
-    @if(Auth::check())
-        <a href="{{ route('dashboard') }}" class="btn btn-outline-light" style="border-radius: 100px; margin-left: 3rem; position: absolute; top: 2rem; right: 2rem;">Escritorio</a>
-    @else
-        <a href="{{ route('login') }}" class="btn btn-outline-light" style="border-radius: 100px; margin-left: 3rem; position: absolute; top: 2rem; right: 2rem;">Iniciar sesión</a>
-    @endif
-</div>
+
 
 </div>
 
